4fb798be15cf743520e6abfdd912ddb0b8c83984
Renee Salz <rsalz@berkeley.edu>
2022-02-11 16:45:40 +0100

    finished script

--- helper_scripts/vcf_to_ind.py
+++ helper_scripts/vcf_to_ind.py
@@ -4,20 +4,6 @@ import pandas as pd
 import argparse
 import io
 
-def parse(file):
-    df=pd.DataFrame()
-    with open(file,'r') as f:
-        for line in f:
-            if not line.startswith('#'):
-                start=line.split('PASS')[0]
-                start=start.split('\t')
-                ident='_'.join([start[0],start[1],start[3]])+'/'+start[4]
-                # count=total_patients-line.count("./.:.:.:.:.")
-                count_hom=line.count("1/1:")
-                count_het=line.count("0/1:")
-                df = df.append({'Uploaded_variation': ident, 'patients_hom':count_hom,'patients_het':count_het}, ignore_index=True)
-    return(df)
-
 def combined_vcf_to_mapping(path):
     '''take the combined vcf file and make a 2 column mapping file of variants to patient IDs who have the variant
     can use this as alternative to '--individual all' on the VEP command line, which prints one line for every patient.
@@ -28,8 +14,12 @@ def combined_vcf_to_mapping(path):
     df['Uploaded_variation']=df['CHROM']+'_'+df['POS'].astype(str)+'_'+df['REF']+'/'+df['ALT'].apply(lambda x: x.replace(',','/') if ',' in x else x)
     df=df.drop(columns=['CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT']).set_index('Uploaded_variation').replace({'./.:.:.:.:.':None})
     pd.DataFrame(df.notna().dot(df.columns+',').str.rstrip(','), index=df.index, columns=['patients']).reset_index().to_csv('variants_to_patients.csv',index=False)
-    # df
- 
 
 def main():
-    parse()
+    parser = argparse.ArgumentParser(description='VEP parser for polyphen-2')
+    parser.add_argument('--vcf', help='vep output', required=True)
+    args=vars(parser.parse_args())
+    combined_vcf_to_mapping(args['vcf'])
+
+if __name__ == '__main__':
+    main()
