fb20c9cf0820803837b908593da512a5697f2390
Renee Salz <rsalz@berkeley.edu>
2022-02-11 16:56:30 +0100

    generalizability

--- run_orfpred_and_vep.sh
+++ run_orfpred_and_vep.sh
@@ -18,5 +18,5 @@ perl $TransDecoderpath/util/gff3_file_to_proteins.pl --gff3 observed_novel_pc.fa
 sed -i 's/*//g' observed_novel_pc.orfs.faa
 grep -v "#" observed_novel_pc.fasta.transdecoder.genome.gff3 | awk NF | sort -k1,1 -k4,4n -k5,5n -t$'\t' | bgzip -c > LR_transdecoder.gff.gz
 tabix -p gff LR_transdecoder.gff.gz
-vep --cache --merged --assembly GRCh38 --cache_version 104 --max_af --sift b --polyphen b --protein --domains --plugin Conservation,/home/renees/.vep/gerp_conservation_scores.homo_sapiens.GRCh38.bw --gff LR_transdecoder.gff.gz --pick_allele --pick_order rank,biotype,length --fasta $genomefile --no_stats --offline --fork 4 --force -i $variantfile -o LR_all_patients.tab
+vep --cache --merged --assembly GRCh38 --cache_version 104 --max_af --sift b --polyphen b --protein --domains --plugin Conservation,$pathtobigwig --gff LR_transdecoder.gff.gz --pick_allele --pick_order rank,biotype,length --fasta $genomefile --no_stats --offline --fork 4 --force -i $variantfile -o LR_all_patients.tab
 python /mnt/xomics/renees/data/host_pathogen_PID/host_pathogen_interactions/reannotation/parse_vep.py --vep LR_all_patients.tab
