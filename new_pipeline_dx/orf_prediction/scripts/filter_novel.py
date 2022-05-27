63b5c7e29c97a17de487ff2a06a48b65a1b68e84
Renee Salz <rsalz@berkeley.edu>
2022-05-27 13:34:33 +0200

    take out argparse

--- new_pipeline_dx/orf_prediction/scripts/filter_novel.py
+++ new_pipeline_dx/orf_prediction/scripts/filter_novel.py
@@ -2,15 +2,13 @@
 
 import pandas as pd
 import gtfparse
+import sys
 
 def main():
-    parser = argparse.ArgumentParser("IO file locations for make pacbio cds gtf")
-    parser.add_argument("--gffcompare_gtf", action="store", dest = "gffcompare_gtf")
-    results = parser.parse_args()
-    combined=gtfparse.read_gtf(results.gffcompare_gtf)
+    combined=gtfparse.read_gtf(sys.argv[1])
     exclude=combined[combined['class_code']=='=']['transcript_id'].unique()
     combined=combined[~combined['transcript_id'].isin(exclude)]
-    df=pd.read_table(results.gffcompare_gtf, names=['seqname','source','feature','start','end','score','strand','frame','extra'])
+    df=pd.read_table(sys.argv[1], names=['seqname','source','feature','start','end','score','strand','frame','extra'])
     df.merge(combined[['seqname','source','feature','start','end']]).to_csv('gffcmp.combined.filtered.gtf',sep='\t',index=False,header=False)
 
 if __name__ == "__main__":
