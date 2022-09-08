048842d27e8bb6b44747678a263d31da6591351a
Renee Salz <rsalz@berkeley.edu>
2022-09-08 11:11:11 +0200

    different seperator for downstream analysis

--- orf_prediction/bin/vcf_to_ind.py
+++ orf_prediction/bin/vcf_to_ind.py
@@ -10,9 +10,11 @@ def combined_vcf_to_mapping(path):
     '''
     with open(path, 'r') as f:
         lines = [l for l in f if not l.startswith('##')]
-    df=pd.read_csv(io.StringIO(''.join(lines)),sep='\t').rename(columns={'#CHROM': 'CHROM'})
-    df=df.drop(columns=['ID','QUAL','FILTER','INFO','FORMAT']).set_index(['CHROM','POS','REF','ALT']).replace({'./.:.:.:.:.':None})
-    pd.DataFrame(df.notna().dot(df.columns+',').str.rstrip(','), index=df.index, columns=['patients']).reset_index().drop_duplicates().to_csv('variants_to_patients.tsv',sep='\t',index=False)
+    df=pd.read_csv(io.StringIO(''.join(lines)),sep='\t').rename(columns={'#CHROM': 'CHROM'}).drop(columns=['ID','QUAL','FILTER','INFO','FORMAT'])
+    df=df.set_index(['CHROM','POS','REF','ALT']).replace(':.*','',regex=True).replace({'./.':None}).apply(lambda x : x.name + ':' + x if x is not None else x)
+    df['patients']=df.apply(lambda x: ';'.join(x.dropna().astype(str)),axis=1)
+    df[['patients']].reset_index().drop_duplicates().to_csv('variants_to_patients.tsv',sep='\t',index=False)
+    #pd.DataFrame(df.notna().dot(df.columns+',').str.rstrip(','), index=df.index, columns=['patients'])
 
 def main():
     parser = argparse.ArgumentParser(description='VEP parser for polyphen-2')
